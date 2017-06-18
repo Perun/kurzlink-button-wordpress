c57e0cc9a8862e080735c66d21ff02738f84ecf5
Vladimir Simović <info@perun.net>
2017-06-18 22:32:28 +0200

    Update kurzlink-button.php
    Meta-Informationen (WP-eigene) hinzugefügt

--- kurzlink-button.php
+++ kurzlink-button.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * Plugin Name: Kurzlink-Button
+ * Plugin URI:  https://www.perun.net/2015/12/14/wordpress-4-4-kurzlink-button-zurueckholen/
+ * Description: Dieses Plugin bringt den Kurzlink-Button oberhalb des Editors zurück
+ * Author:      Vladimir Simović
+ * Author URI:  https://www.vladimir-simovic.de
+ * Version:     1.0.0
+ * Text Domain: kurzlink-button
+ * License:     GPLv3
+ */
+
 // Kurzlink-Button zurückholen
 if ( function_exists( 'add_filter' ) ) {
     add_filter( 'get_shortlink', function ( $shortlink ) {
